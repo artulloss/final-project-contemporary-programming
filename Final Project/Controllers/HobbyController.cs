42978349fcf26d4b5b84ae7c9e773148fdd11a0f
Zach <pittmanzach2@gmail.com>
2023-04-24 13:52:13 -0400

    Implemented MemberInfo & Hobby Controllers

--- Final Project/Controllers/HobbyController.cs
+++ Final Project/Controllers/HobbyController.cs
@@ -34,5 +34,72 @@ namespace Final_Project.Controllers
 
             return hobby;
         }
+
+        [HttpPost]
+        public async Task<ActionResult<Hobby>> CreateHobby(Hobby hobby)
+        {
+            _context.Hobbies.Add(hobby);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetHobby), new { id = hobby.Id }, hobby);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Hobby>> DeleteHobby(int id)
+        {
+            var hobby = await _context.Hobbies.FindAsync(id);
+
+            if(hobby == null)
+            {
+                return NotFound();
+            }
+
+            _context.Hobbies.Remove(hobby);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Hobby>> UpdateHobby(int id, Hobby hobby)
+        {
+            if (id != hobby.Id)
+            {
+                return BadRequest();
+            }
+
+            if(!HobbyExists(id))
+            {
+                _context.Hobbies.Add(hobby);
+            }
+            else
+            {
+                _context.Entry(hobby).State = EntityState.Modified;
+            }
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+
+            if(!HobbyExists(id))
+            {
+                return CreatedAtAction(nameof(GetHobby), new {id = hobby.Id }, hobby);
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+
+        private bool HobbyExists(int id)
+        {
+            return _context.Hobbies.Any(e => e.Id == id);
+        }
     }
 }
--- Final Project/Controllers/MemberInfoController.cs
+++ Final Project/Controllers/MemberInfoController.cs
@@ -34,5 +34,72 @@ namespace Final_Project.Controllers
 
             return info;
         }
-    }
+
+        [HttpPost]
+        public async Task<ActionResult<MemberInfo>> CreateMemberInfo(MemberInfo memberInfo)
+        {
+            _context.MemberInfos.Add(memberInfo);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(CreateMemberInfo), new { id = memberInfo.Id }, memberInfo);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<MemberInfo>> DeleteMemberInfo(int id)
+        {
+            var memberInfo = await _context.MemberInfos.FindAsync(id);
+
+            if (memberInfo == null) 
+            {
+                return NotFound();
+            }
+
+            _context.MemberInfos.Remove(memberInfo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<MemberInfo>> UpdateMemberInfo(int id, MemberInfo memberInfo)
+        {
+            if (id != memberInfo.Id)
+            {
+                return BadRequest();
+            }
+
+            if (!MemberInfoExists(id))
+            {
+                _context.MemberInfos.Add(memberInfo);
+            }
+            else
+            {
+                _context.Entry(memberInfo).State = EntityState.Modified;
+            }
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+
+            if (!MemberInfoExists(id))
+            {
+                return CreatedAtAction(nameof(GetMemberInfo), new { id = memberInfo.Id }, memberInfo);
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+
+        private bool MemberInfoExists(int id)
+        {
+            return _context.MemberInfos.Any(e => e.Id == id);
+        }
+    }    
 }
